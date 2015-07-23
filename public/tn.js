1e00f233640d2ee522045d1b894d03eb25bcbe05
Jeffrey Rodowicz <jrodowicz@trialnetworks.com>
2015-07-23 08:45:07 -0400

    Added a selector!

--- public/tn.js
+++ public/tn.js
@@ -1,4 +1,7 @@
 // selector code
+window.$ = function(selector) {
+    return document.querySelectorAll(selector);              // The returns everything with attributes matching elements
+}
 
 // event code
 
